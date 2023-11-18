8e32f38a1313e982461a4d61a0d4a0540e0a12f5
SK Arbaj Ali <skarbajali.one@outlook.com>
2023-11-18 16:16:27 +0530

    Feature: search and data fetching functionality implemented.

--- src/components/MainWindow.jsx
+++ src/components/MainWindow.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import SearchComponent from './SearchComponent';
 import WordComponent from './WordComponent';
 
+let url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
 const MainWindow = () =>{
+
+    const [fetchedWordData, setFetchedWordData] = React.useState(null);
+    const handleDataFetching = (word) =>{
+        fetch(`${url}${word}`)
+            .then(response => response.json())
+            .then(jsonData => {setFetchedWordData(jsonData);console.log(jsonData);})
+            .catch(e => console.log(e));
+    }
+
     return (
         <div className="main-window">
-            <SearchComponent />
+            <SearchComponent handleDataFetching={handleDataFetching} />
             <WordComponent />
         </div>
     );
--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import searchIcon from '../assets/icons8-search-128.png';
 
-const SearchComponent = () =>{
+const SearchComponent = ({handleDataFetching}) =>{
+
+    const [inputWord, setInputWord] = React.useState("");
+    const handleInputWordChange = (event) =>{
+        setInputWord(event.target.value);
+    };
+
     return (
         <div className='search-Component'>
             <label>
-                <input type="text" name="search" placeholder="Type your text here . . ." />
+                <input 
+                    type="text" 
+                    name="search" 
+                    placeholder="Type your text here . . ."
+                    value={inputWord}
+                    onChange={handleInputWordChange}
+                />
             </label>
-            <img src={searchIcon} alt="Search Icon" />
+            <img src={searchIcon} alt="Search Icon" width="40px" onClick={()=>{handleDataFetching(inputWord)}} />
         </div>
     );
 };
