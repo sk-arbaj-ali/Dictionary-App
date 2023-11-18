b7a2c9ed4b131c3b070edfc7126175ad232d925f
SK Arbaj Ali <skarbajali.one@outlook.com>
2023-11-18 15:39:09 +0530

    Feature: MarkUp for OriginOfWord Component created. Remarks: Style should be better.

---
+++ src/components/OriginOfWord.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OriginOfWord = () =>{
+    return (
+        <div className="origin-of-word" style={styles}>
+            <span>Origin: </span>
+            <em>Origin of the word</em>
+        </div>
+    );
+};
+
+export default OriginOfWord;
+
+let styles = {border:"2px solid black",marginTop:"2px",marginBottom:"2px",};
